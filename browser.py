2785e9c2e662b19c1ee1a255c4348994068d783f
Alan D Moore <me@alandmoore.com>
2012-05-31 10:20:53 -0500

    - migrated from configobj to yaml for configuration language     - New fully-commented configuration file wcgbrowser.yaml     - Fullscreen and navigation can be toggled in the config file as well as the CLI     - Python 3 compatible     - Fixed bug where timeout and hidden quit weren't set up if navigation was hidden     - timeout is now set in seconds, not milliseconds

--- browser.py
+++ browser.py
@@ -6,8 +6,9 @@ from PyQt4.QtWebKit import QWebView, QWebPage, QWebSettings
 import sys
 import os
 import argparse
-from configobj import ConfigObj
-    
+#rom configobj import ConfigObj
+import yaml
+
 class MainWindow(QMainWindow):
     def createAction(self, text, slot=None, shortcut=None, icon=None, tip=None, checkable=False, signal="triggered()"):
         """Borrowed from 'Rapid GUI Development with PyQT by Mark Summerset'"""
@@ -29,18 +30,18 @@ class MainWindow(QMainWindow):
     def onLinkClick(self, url):
         if DEBUG:
             if not url.isValid():
-                print "Invalid URL %s" % url
+                print("Invalid URL %s" % url)
             else:
-                print "Load URL %s" %url
+                print("Load URL %s" %url)
                                
     def __init__(self, options, parent = None):
         super(MainWindow, self).__init__(parent)
         #Load config file
         self.options = options
-        self.configuration = ConfigObj(self.options.config_file)
+        self.configuration = yaml.safe_load(open(self.options.config_file, 'r'))
         if DEBUG:
-            print "loading configuration from '%s'" % options.config_file
-            print self.configuration
+            print("loading configuration from '%s'" % options.config_file)
+            print(self.configuration)
         self.build_ui(self.options, self.configuration)
         self.html404 = """<h2>Unavailable</h2>
         <p>You have attempted to navigate to a page which is not accessible from this terminal.</p>
@@ -55,11 +56,14 @@ class MainWindow(QMainWindow):
         <p>If you continue to get this error, contact technical support</p> """ % (self.startUrl)
 
     def build_ui(self, options, configuration):
-        self.startUrl = options.url or configuration.get("start_url") or "about:blank"
-        inactivity_timeout = options.timeout or int(configuration.get("timeout") or 0)
-        self.icon_theme = options.icon_theme or configuration.get("icon_theme") or None
+        self.startUrl = options.url or configuration.get("start_url", "about:blank") 
+        inactivity_timeout = options.timeout or int(configuration.get("timeout", 0))
+        self.icon_theme = options.icon_theme or configuration.get("icon_theme", None)
         self.zoomfactor = options.zoomfactor or float(configuration.get("zoom_factor") or 1.0)
-        self.allowPopups = options.allowPopups or configuration.get("allow_popups") or False
+        self.allowPopups = options.allowPopups or configuration.get("allow_popups", False) 
+        self.isFullscreen = options.isFullscreen or configuration.get("fullscreen", False) 
+        self.showNavigation = not options.noNav and configuration.get('navigation', True)
+        
         ###Start GUI configuration###
         self.browserWindow = WcgWebView(allowPopups=self.allowPopups)
         self.browserWindow.settings().setAttribute(QWebSettings.JavascriptCanOpenWindows, self.allowPopups)
@@ -79,12 +83,12 @@ class MainWindow(QMainWindow):
             QIcon.setThemeName(self.icon_theme)
         self.setCentralWidget(self.browserWindow)
         if DEBUG:
-            print options
-            print "loading %s" % self.startUrl
+            print (options)
+            print ("loading %s" % self.startUrl)
         self.browserWindow.setUrl(QUrl(self.startUrl))
-        if options.isFullscreen is True:
+        if self.isFullscreen is True:
             self.showFullScreen()
-        if options.noNav is not True:
+        if self.showNavigation is True:
             self.navigationBar = QToolBar("Navigation")
             self.addToolBar(Qt.TopToolBarArea, self.navigationBar)
             self.navigationBar.setMovable(False)
@@ -96,22 +100,20 @@ class MainWindow(QMainWindow):
             self.quit = self.createAction("I'm &Finished", self.reset_browser, QKeySequence("Alt+F"), None, "Click here when you are done to clear your browsing history and reset the search.")
             self.zoom_in_button = self.createAction("Zoom In", self.zoom_in, QKeySequence("Alt++"), "zoom-in", "Increase the size of the text and images on the page")
             self.zoom_out_button = self.createAction("Zoom Out", self.zoom_out, QKeySequence("Alt+-"), "zoom-out", "Decrease the size of text and images on the page")
-            self.really_quit = self.createAction("", self.close, QKeySequence("Ctrl+Alt+Shift+Q"), None, "")
             self.navigationBar.addAction(self.back)
             self.navigationBar.addAction(self.forward)
             self.navigationBar.addAction(self.refresh)
             self.navigationBar.addAction(self.stop)
             self.navigationBar.addAction(self.zoom_in_button)
             self.navigationBar.addAction(self.zoom_out_button)
-            self.addAction(self.really_quit)
             
             self.navigationBar.addSeparator()
             #Insert bookmarks buttons here.
             self.bookmark_buttons = []
             if configuration.get("bookmarks"):
-                for bookmark in configuration.get("bookmarks").iteritems():
+                for bookmark in configuration.get("bookmarks").items():
                     if DEBUG:
-                        print "%s, %s, %s\n" % (bookmark[0], bookmark[1]["url"], bookmark[1]["description"])
+                        print("%s, %s, %s\n" % (bookmark[0], bookmark[1]["url"], bookmark[1]["description"]))
                     button = self.createAction(bookmark[0],
                                            lambda url=bookmark[1].get("url"): self.browserWindow.load(QUrl(url)),
                                            QKeySequence.mnemonic(bookmark[0]),
@@ -128,13 +130,18 @@ class MainWindow(QMainWindow):
             #This removes the ability to toggle off the navigation bar:
             self.navToggle = self.navigationBar.toggleViewAction()
             self.navToggle.setVisible(False)
+            #End "if showNavigation is True" block
 
-            #Call a reset function after timeout
-            if inactivity_timeout != 0:
-                self.ef = Inactivity_Filter(inactivity_timeout)
-                self.installEventFilter(self.ef)
-                self.browserWindow.page().installEventFilter(self.ef)
-                self.connect(self.ef, SIGNAL("timeout()"), self.reset_browser)
+        #set hidden quit action    
+        self.really_quit = self.createAction("", self.close, QKeySequence("Ctrl+Alt+Shift+Q"), None, "")
+        self.addAction(self.really_quit)
+
+        #Call a reset function after timeout
+        if inactivity_timeout != 0:
+            self.ef = Inactivity_Filter(inactivity_timeout)
+            self.installEventFilter(self.ef)
+            self.browserWindow.page().installEventFilter(self.ef)
+            self.connect(self.ef, SIGNAL("timeout()"), self.reset_browser)
 
         
         ###CONNECTIONS### 
@@ -155,7 +162,7 @@ class MainWindow(QMainWindow):
         if DEBUG:
             print ("SSL error ignored")
             for error in errorList:
-                print error.errorString()
+                print(error.errorString())
 
     def onLoadFinished(self, ok):
         if not ok:
@@ -183,7 +190,7 @@ class MainWindow(QMainWindow):
 class Inactivity_Filter(QTimer):
     def __init__(self, timeout=0, parent=None):
         super(QTimer, self).__init__(parent)
-        self.timeout = timeout
+        self.timeout = timeout * 1000 #timeout needs to be converted from seconds to milliseconds
         self.setInterval(self.timeout)
         self.start()
         
@@ -225,25 +232,25 @@ def main(args):
 
 
 if __name__ == "__main__":
-    if os.path.isfile(os.path.expanduser("~/.wcgbrowser.conf")):
-        default_config_file =  os.path.expanduser("~/.wcgbrowser.conf")
-    elif os.path.isfile("/etc/wcgbrowser.conf"):
-        default_config_file = "/etc/wcgbrowser.conf"
+    if os.path.isfile(os.path.expanduser("~/.wcgbrowser.yaml")):
+        default_config_file =  os.path.expanduser("~/.wcgbrowser.yaml")
+    elif os.path.isfile("/etc/wcgbrowser.yaml"):
+        default_config_file = "/etc/wcgbrowser.yaml"
     else:
         default_config_file = None
     parser = argparse.ArgumentParser()
-    parser.add_argument("-l", "--url", action="store", type=unicode, dest="url", help="start browser at URL")
+    parser.add_argument("-l", "--url", action="store", dest="url", help="start browser at URL")
     parser.add_argument("-f", "--fullscreen", action="store_true", default=False, dest="isFullscreen", help="start browser FullScreen")
     parser.add_argument("-n", "--no-navigation", action="store_true", default=False, dest="noNav", help="start browser without Navigation controls")
     parser.add_argument("-c", "--config-file", action="store", default=default_config_file, dest="config_file", help="Specifiy an alternate config file")
     parser.add_argument("-d", "--debug", action="store_true", default=False, dest="DEBUG", help="enable debugging output")
-    parser.add_argument("-t", "--timeout", action="store", type=int,  default=0, dest="timeout", help="define the timeout after which to reset the browser due to user inactivity")
+    parser.add_argument("-t", "--timeout", action="store", type=int,  default=0, dest="timeout", help="define the timeout in seconds after which to reset the browser due to user inactivity")
     parser.add_argument("-i", "--icon-theme", action="store", default=None, dest="icon_theme", help="override default icon theme with other Qt/KDE icon theme")
     parser.add_argument("-z", "--zoom", action="store", type=float, default=0, dest="zoomfactor", help="set the zoom factor for web pages")
     parser.add_argument("-p", "--popups", action="store_true", default=False, dest="allowPopups", help="allow the browser to open new windows")
     args = parser.parse_args()
     if not args.config_file:
-        print "No config file found or specified; using defaults."
+        print ("No config file found or specified; using defaults.")
     DEBUG = args.DEBUG
     main(args)
 
